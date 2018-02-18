d50942bc7414c9d76cb3d58cd2c960ba21750042
ggabee2k <36583157+ggabee2k@users.noreply.github.com>
2018-02-18 19:08:33 +0900

    new added

---
+++ rtp.c
@@ -0,0 +1,12 @@
+/* includes */
+
+#include <stdio.h>
+
+int main (
+    int	   argc,	/* number of arguments */
+    char * argv[]	/* array of arguments */
+    ) {
+	printf("Hello!!\n");
+
+    return 0;
+}
